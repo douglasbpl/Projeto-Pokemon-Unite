34780163463d78e21f5442bddda2e278811351cd
douglasbpl <douglas.bpl94@yahoo.com.br>
2021-12-07 17:49:07 -0300

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Pokemon Unite",
   },
-  plugins: ["gatsby-plugin-styled-components"],
-  plugins: [`gatsby-plugin-react-helmet`],
-  plugins: ['gatsby-plugin-netlify'],
-  plugins: [
-    {
-      resolve: "gatsby-plugin-anchor-links",
-      options: {
-        offset: -100
-      }
-    }
-  ],
-   plugins: [ {
+  plugins: ["gatsby-plugin-styled-components",
+  {
       resolve: "gatsby-source-graphql",
       options: {
                 typeName: "alldata",
@@ -22,5 +12,5 @@ module.exports = {
                 url: "https://api-us-east-1.graphcms.com/v2/ckvtiw36d0bqn01xv7cst26b8/master",
       },
     },
-  ]
+  ],
   }
